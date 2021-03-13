5b5be08484b399a20c6472d432e60c2f11d8146b
Conner Vieira <connervieira@tutanota.com>
2021-03-12 22:58:30 -0500

    Added HealthBox support

---
+++ CHANGELOG.md
@@ -0,0 +1,18 @@
+# Breathe Changelog
+
+## Version 1.0
+
+### Initial version
+
+- Basic functionality
+
+## Version 2.0
+
+### HealthBox Support
+
+- Full support for HealthBox 'Mindful Minutes' metric
+
+### Subtle Improvements
+
+- Slightly adjusted timing
+- Better code comments
--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 # Breathe
 # V0LT
 # Licensed under the GPLv3
-# Version 1.0 
+# Version 2.0 
 
+
+# ----- Configuration -----
+
+# Defines whether or not you'd like to submit the time you spend breathing to HealthBox as 'Mindful Minutes' (Metric B6). If this setting is changed, you'll need to configure your HealthBox instance information below.
+use_healthbox = False
+
+# Defines the host address and port that you host HealthBox on. If you're running HealthBox locally on it's default port, it will be 'localhost:5050'
+healthbox_server = "localhost:5050"
+
+# This is the API key that Breathe will use to communicate with HealthBox. This key needs to have 'source' permissions, and be allowed to access metric B6 (Mindful Minutes)
+healthbox_apikey = ""
+
+# This variable determines whether or not the URL will be printed when making the network request to HealthBox. This is extremely useful for debugging, but can be messy during normal usage. This should be left as 'False' if you aren't a developer.
+url_debugging = False
+
+# ----- End Of Configuration -----
+
+
+
+# Import required Python modules
 import gi
 import threading
 gi.require_version('Gtk', '3.0')
@@ -11,6 +31,40 @@ from gi.repository import Gdk
 from gi.repository import Gio
 import sys
 import random
+from datetime import datetime
+import traceback
+import urllib.parse
+
+
+# Attempt to import the 'requests' module
+try:
+    import requests
+except ModuleNotFoundError as error:
+    if traceback.format_exception_only (ModuleNotFoundError, error) != ["ModuleNotFoundError: No module named 'requests'\n"]: # Make sure the error we're catching is the right one
+        raise # If not, raise the error
+    raise utils.MissingLibraryError ("making web server requests", "requests")
+
+
+
+# Define the function required to make network requests to HealthBox
+class APICallError (Exception): pass # This will be used when returning errors.
+
+def make_request (*, server, api_key, submission = None, print_url = False):
+    endpoint = "metrics/b6/submit".split ('/') # Define the method and metric that this request will use on HealthBox.
+    url = f"http://{server}/api/source/{'/'.join (endpoint)}?api_key={urllib.parse.quote (api_key)}" # Form the URL that will be used to communicate with HealthBox
+    if submission is not None:
+        url += f"&submission={urllib.parse.quote (submission)}" # Attach the JSON submission data to the URL formed above.
+    if print_url: print (f"Making a request to {url}")
+    response = requests.get (url) # Send the network request.
+    response_data = response.json () # Save the response of the network request to the response_data variable.
+    if not response_data ["success"]: # If something goes wrong, return an error.
+        raise APICallError (response_data ["error"])
+    del response_data ["success"]
+    del response_data ["error"]
+    return response_data
+
+
+
 
 class Main(Gtk.ApplicationWindow): # This is the class for the main application window. This is the first window shown when launching the application. 
     def __init__(self, app):
@@ -36,7 +90,7 @@ class Breathing(Gtk.ApplicationWindow):
     
         self.increment_value = 0 # Initialize increment value variable to be used in the breathing animation later. 
 
-        listbox = Gtk.ListBox() # Create any empty list box to organize UI elements
+        listbox = Gtk.ListBox() # Create any empty list box to organize UI elements.
         self.breathing_bar = Gtk.ProgressBar() # Create a progress bar to visualize breathing.
 
         listbox.add(self.breathing_bar) # Add the breathing progress bar to the listbox.
@@ -50,15 +104,35 @@ class Breathing(Gtk.ApplicationWindow):
         self.show_all() # Show all elements in the user interface.
 
         def StartBreathing(): # Called when the guided breathing starts.
+            self.start_time = int(datetime.timestamp(datetime.now())) # Save the time when the breathing exercise started to a variable.
             self.timeout_id = GLib.timeout_add(5, self.Animation, None)
 
         def StopBreathing(): # Called to stop the breathing exercise.
+            end_time = int(datetime.timestamp(datetime.now())) # Save the time when the breathing exercise stopped to a variable.
             GLib.source_remove(self.timeout_id) # Stop the breathing progress bar animation started by StartBreathing()
             self.breathing_direction.set_markup("<span font_desc='Lato Light 40'>%s</span>" % "Good job!") # Display "Good job" message
             self.breathing_bar.set_fraction(0) # Set progress bar to 0. 
+            
+            if (use_healthbox == True):
+
+                if (healthbox_server == ""): # Check to see if the user has entered a server address and port. If not, display an error.
+                    print("Error: You've configured Breathe to use HealthBox, but you haven't entered the server address of your HealthBox instance. Please do so in the configuration at the top of 'main.py'.")
+
+                elif (healthbox_apikey == ""): # Check to see if the user has entered an API key. If not, display an error.
+                    print("Error: You've configured Breathe to use HealthBox, but you haven't entered an API key for Breathe to use. Please do so in the configuration at the top of 'main.py'.")
+
+                else: # If the user has entered an API key and server address, attempt to submit the breathing session to HealthBox.
+
+                    # Generate the submission data as plain text JSON data.
+                    submission = '{"timestamp": ' + str(end_time) + ', "data": {"type_of_mindfulness": "Breathing", "start_time": ' + str(self.start_time) + ', "end_time": ' + str(end_time) + '}}'
+
+                    # Form and send the network request that will be used to submit the information to HealthBox using the make_request function.
+                    response = make_request (server = healthbox_server, submission = submission, api_key = healthbox_apikey, print_url = url_debugging)
+
+
 
         threading.Timer(6.0, StartBreathing).start() # Start the breathing exercises 6 seconds after the window has opened, giving the user time to read the instructions.
-        threading.Timer(66.0, StopBreathing).start() # Stop breathing exercises after 66 seconds (60 seconds of breathing plus the 6 second timer above).
+        threading.Timer(67.0, StopBreathing).start() # Stop breathing exercises after 67 seconds (62 seconds of breathing plus the 5 second timer above).
 
     def Animation(self, user_data):
         current_value = self.breathing_bar.get_fraction() # Define current_value as the current progress of the progress bar (0.0 through 1.0)
